652505f3b86ae96c638d4755db9debf4a55c8ff0
Joansitoh <joansitohprah@gmail.com>
2024-03-05 16:30:43 +0100

    Images

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from PIL import Image, ImageDraw, ImageFont
+import os
 # from utils import generate_grid
 
 WHITEBOARD_PATH = "assets/whiteboard.png"
@@ -7,6 +8,19 @@ OUTPUT_PATH = "dist/"
 GRID_SIZE = 50
 
 COUNTER_PATH = "assets/counter.txt"
+
+
+class Step:
+
+    def __init__(self, title, image=None):
+        self.title = title
+        self.image = image
+
+    def get_title(self):
+        return self.title
+    
+    def get_image(self):
+        return self.image
 
 def get_font(size):
     if FONT_PATH:
@@ -66,27 +80,49 @@ def generate_image(message, steps, color=(0, 0, 0), size=20):
         paint.text((x_position, 75), message, fill=color, font=font)
     
     y_position = 150
+
+    x_image_pos = 460
+    y_image_pos = 300
+    image_pos_up = False
     for step in steps:
-        while step:
-            cut_step = cut_text_to_fit(step, 1000 - x_position, FONT_PATH, size)
+        while step.get_title():
+            cut_step = cut_text_to_fit(step.get_title(), 1000 - x_position, FONT_PATH, size)
             paint.text((x_position, y_position), cut_step, fill=(0, 0, 0), font=font)
-            step = step[len(cut_step):]
+            step_title = step.get_title()[len(cut_step):]
+            step = Step(step_title, step.get_image())
             x_position = 390
             y_position += size 
 
+        if step.get_image():
+            print(step.get_image())
+            step_img = Image.open(step.get_image())
+
+            if step_img.size[0] > 200:
+                step_img = step_img.resize((200, int(step_img.size[1] * (200 / step_img.size[0]))))
+
+            img.paste(step_img, (x_image_pos, y_image_pos)) 
+
+            image_pos_up = not image_pos_up
+            x_image_pos += step_img.size[0] - 40
+            if image_pos_up:
+                y_image_pos += step_img.size[1] + 20
+            else:
+                y_image_pos -= step_img.size[1] + 20
+
     counter = str(count_one()).zfill(5)
+    os.makedirs(OUTPUT_PATH, exist_ok=True)
+
     path = OUTPUT_PATH + f"croquis-{counter}.png"
     img.save(path)
 
     return path
 
-
 generate_image(
     "Titulo de lo que sea",
     [
-        "1. Que onda waxo",
-        "2. Hello, im your friend :_d",
-        "3. Oh la la, bonjour mon ami",
+        Step("1. Que onda waxo", "assets/1.png"),
+        Step("2. Hello, im your friend :_d", "assets/1.png"),
+        Step("3. Oh la la, bonjour mon ami", "assets/1.png"),
     ],
     (0, 0, 0),
     20
